cb058b87ee870b27fb7a403e31da988418c34e1f
5arias <stephen.brody@gmail.com>
2018-04-30 11:35:56 -0400

    config react hot loader

--- src/index.dev.js
+++ src/index.dev.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { hot } from 'react-hot-loader'
 
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const HotApp = hot(module)(App)
+
+ReactDOM.render(<HotApp />, document.getElementById('root'))
