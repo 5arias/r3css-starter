409f1bc461be3fff097dc16221f037d1c6995ff2
5arias <stephen.brody@gmail.com>
2018-04-30 16:07:17 -0400

    connect a component to redux and verify RHL still works

--- src/index.dev.js
+++ src/index.dev.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import { configureStore } from './redux/store'
-import App from './App'
+import { App } from './containers/app'
 import './index.css'
 
 const store = configureStore()
